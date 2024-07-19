5a14f2e2320ff3cf2f2fcec6781636954d56411e
kwsc98 <958280102@qq.com>
2024-07-19 17:46:26 +0800

    init

--- suanleme-common/src/business.rs
+++ suanleme-common/src/business.rs
@@ -12,3 +12,43 @@ pub struct CommonResponse<T> {
     message: Option<String>,
     data: Option<T>,
 }
+
+impl<T: Default> CommonRequest<T> {
+    pub fn buildr() -> Self {
+        CommonRequest::default()
+    }
+    pub fn user_id(mut self, user_id: Option<String>) -> Self {
+        self.user_id = user_id;
+        self
+    }
+    pub fn token(mut self, token: Option<String>) -> Self {
+        self.token = token;
+        self
+    }
+    pub fn sign_str(mut self, sign_str: Option<String>) -> Self {
+        self.sign_str = sign_str;
+        self
+    }
+    pub fn data(mut self, data: Option<T>) -> Self {
+        self.data = data;
+        self
+    }
+}
+
+impl<T: Default> CommonResponse<T> {
+    pub fn buildr() -> Self {
+        CommonResponse::default()
+    }
+    pub fn code(mut self, code: String) -> Self {
+        self.code = code;
+        self
+    }
+    pub fn message(mut self, message: Option<String>) -> Self {
+        self.message = message;
+        self
+    }
+    pub fn data(mut self, data: Option<T>) -> Self {
+        self.data = data;
+        self
+    }
+}
