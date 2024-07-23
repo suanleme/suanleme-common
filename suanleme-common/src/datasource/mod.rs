47942b30e4a52c415b39247497d48ab0e109b5e2
kwsc98 <958280102@qq.com>
2024-07-23 18:16:18 +0800

    20240723

--- suanleme-common/src/datasource/mod.rs
+++ suanleme-common/src/datasource/mod.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
-use sqlx::{Error, PgPool};
+use sqlx::{query, Error, PgConnection, PgPool};
+
+use crate::error::BoxError;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DataSourceConfig {
@@ -13,4 +15,14 @@ pub async fn init_data_source(config: &DataSourceConfig) -> Result<PgPool, Error
     url = url.replace("{username}", &config.username);
     url = url.replace("{password}", &config.password);
     PgPool::connect(&url).await
+}
+
+pub async fn set_constraints_all_immediate(connect: &mut PgConnection) -> Result<(), BoxError> {
+    let result = query("SET CONSTRAINTS ALL IMMEDIATE")
+        .execute(connect)
+        .await?;
+    if result.rows_affected() != 1 {
+        return Err("set constraints all immediate".into());
+    }
+    Ok(())
 }
