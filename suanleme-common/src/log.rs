51632fec89d2243446a576076cf79051314682cb
kwsc98 <958280102@qq.com>
2024-07-26 11:13:26 +0800

    20240726

--- suanleme-common/src/log.rs
+++ suanleme-common/src/log.rs
@@ -7,6 +7,7 @@ use tracing_appender::rolling::{RollingFileAppender, Rotation};
 pub struct LogConfig {
     pub level: String,
     pub path: String,
+    pub devmode: Option<bool>,
 }
 
 impl LogConfig {
@@ -21,14 +22,17 @@ impl LogConfig {
 }
 
 pub fn init_log(log_config: &LogConfig, app_name: &str) {
-    let file_appender = RollingFileAppender::new(Rotation::DAILY, &log_config.path, app_name);
-    tracing_subscriber::fmt()
+    let tracing = tracing_subscriber::fmt()
         .json()
         .with_max_level(tracing::Level::from_str(&log_config.level).expect("logging init error"))
-        .with_writer(file_appender)
         .with_line_number(true)
-        .with_thread_ids(true)
-        .init();
+        .with_thread_ids(true);
+    if log_config.devmode.is_some_and(|e| !e) || log_config.devmode.is_none() {
+        let file_appender = RollingFileAppender::new(Rotation::DAILY, &log_config.path, app_name);
+        tracing.with_writer(file_appender).init();
+    } else {
+        tracing.init();
+    };
 }
 
 pub fn get_uuid() -> String {
