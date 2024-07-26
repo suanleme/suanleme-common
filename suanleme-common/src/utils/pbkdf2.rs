95aab3467fdca3b06465e4f38c859cac5b1fb665
kwsc98 <958280102@qq.com>
2024-07-26 10:58:27 +0800

    新增pbkdf2hash算法

--- suanleme-common/src/utils/pbkdf2.rs
+++ suanleme-common/src/utils/pbkdf2.rs
@@ -1,8 +1,7 @@
+use crate::error::BoxError;
 use base64::{prelude::BASE64_STANDARD, Engine};
 use ring::pbkdf2;
 use std::num::NonZeroU32;
-
-use crate::error::BoxError;
 
 pub fn pbkdf2_verify(source: &str, target: &str) -> Result<(), BoxError> {
     let target: Vec<&str> = target.split('$').collect();
@@ -22,7 +21,29 @@ pub fn pbkdf2_verify(source: &str, target: &str) -> Result<(), BoxError> {
     .map_err(|e| e.to_string().into())
 }
 
+pub fn pbkdf2_derive(salt: &str, iterations: u32, target: &str) -> Result<String, BoxError> {
+    let mut out = [0; 32];
+    pbkdf2::derive(
+        pbkdf2::PBKDF2_HMAC_SHA256,
+        NonZeroU32::new(iterations).unwrap(),
+        salt.as_bytes(),
+        target.as_bytes(),
+        &mut out,
+    );
+    Ok(BASE64_STANDARD.encode(out))
+}
+
 #[test]
 fn test() {
-    println!("{:?}", pbkdf2_verify("123456", "pbkdf2_sha256$720000$y3th6IpEPZ9OUWoSnXpMrG$kntSUNtJ8vZaHnhyTrPrsadlm8ePQX7g3lhwKR/QbJY="));
+    let target = "123456";
+    let rng_str = crate::utils::token::get_rng_str(22);
+    let derive_str = pbkdf2_derive(&rng_str, 720000, target).unwrap();
+    println!("{}", derive_str);
+    println!(
+        "{:?}",
+        pbkdf2_verify(
+            target,
+            &format!("pbkdf2_sha256$720000${}${}", rng_str, derive_str)
+        )
+    );
 }
--- suanleme-common/src/utils/token.rs
+++ suanleme-common/src/utils/token.rs
@@ -1,10 +1,21 @@
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
+
 use crate::{log::get_uuid, utils::date_util::get_now_date_time};
 
 pub fn get_token() -> String {
     format!("{}-{}", get_uuid(), get_now_date_time())
 }
 
+pub fn get_rng_str(len: usize) -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(len)
+        .map(char::from)
+        .collect()
+}
+
 #[test]
 fn test() {
     println!("{:?}", get_token());
+    println!("{:?}", get_rng_str(22));
 }
