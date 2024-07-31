f4eb3ddf975f992d42071061d2e3d87eb9db3457
kwsc98 <958280102@qq.com>
2024-07-31 16:17:05 +0800

    去除commonRequest的T:Default

--- suanleme-common/src/business.rs
+++ suanleme-common/src/business.rs
@@ -3,7 +3,7 @@ use suanleme_macro::Data;
 use crate::utils::date_util::get_now_date_time_as_millis;
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize, Data)]
-pub struct CommonRequest<T: Default> {
+pub struct CommonRequest<T> {
     version: Option<String>,
     user_id: Option<i64>,
     token: Option<String>,
@@ -13,7 +13,7 @@ pub struct CommonRequest<T: Default> {
 }
 
 #[derive(Debug, Default, Clone, serde::Serialize, serde::Deserialize, Data)]
-pub struct CommonResponse<T: Default> {
+pub struct CommonResponse<T> {
     code: String,
     message: Option<String>,
     data: Option<T>,
