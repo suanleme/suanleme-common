904cb2a1074216e93b81bd60e52eac1a32f3ff9b
kwsc98 <kwsc98@gmail.com>
2025-03-26 17:14:19 +0800

    20240326

---
+++ suanleme-common/src/support/job.rs
@@ -0,0 +1,52 @@
+use fusen_rs::fusen_common::date_util::get_now_date_time_as_millis;
+use opentelemetry::trace::TraceContextExt;
+use std::time::Duration;
+use tokio::task;
+use tracing::{error, info, info_span};
+use tracing_futures::Instrument;
+use tracing_opentelemetry::OpenTelemetrySpanExt;
+
+use crate::FusenFuture;
+
+pub async fn cron_job(
+    interval: u64,
+    job_name: &'static str,
+    future: impl Fn() -> FusenFuture<()> + Clone + Send + 'static,
+) {
+    tokio::spawn(async move {
+        loop {
+            let span = info_span!("base_span");
+            let span_context = span.context();
+            let new_span = info_span!(
+                "corn_job",
+                name = job_name.to_owned(),
+                trace_id = span_context.span().span_context().trace_id().to_string()
+            );
+            new_span.set_parent(span_context);
+            let trace_id = new_span
+                .context()
+                .span()
+                .span_context()
+                .trace_id()
+                .to_string();
+            new_span.set_attribute("trace_id", trace_id.to_owned());
+            let future = future.clone();
+            match task::spawn(
+                async move {
+                    let start_time = get_now_date_time_as_millis();
+                    future().await;
+                    info!(ttl = get_now_date_time_as_millis() - start_time, "done");
+                }
+                .instrument(new_span),
+            )
+            .await
+            {
+                Ok(_) => {}
+                Err(error) => {
+                    error!("cron_job:error:{} error: {}", job_name, error);
+                }
+            };
+            tokio::time::sleep(Duration::from_secs(interval)).await;
+        }
+    });
+}
--- suanleme-common/src/support/mod.rs
+++ suanleme-common/src/support/mod.rs
@@ -1,1 +1,2 @@
 pub mod fusen;
+pub mod job;
